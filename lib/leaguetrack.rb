8f132222eaff0613064cb78b5f1f7ee342769638
Alex Larabie <alexjlarabie@gmail.com>
2025-04-17 20:45:18 -0400

    add file storage in JSON, retrieval and parsing

--- lib/leaguetrack.rb
+++ lib/leaguetrack.rb
@@ -3,48 +3,37 @@
 require_relative "leaguetrack/version"
 require_relative "lapi"
 require_relative "leaguetrack/cli"
+require_relative "leaguetrack/storage"
 require "json"
 
 module Leaguetrack
   class App
     def initialize(account_name, tag)
       @summoner = account_name.to_s + "/" + tag
+      @storage_handler = Leaguetrack::Storage.new()
+      @user = nil
     end
 
     def run
-      # TODO: CHECK IF SUMMONER STORED IN SOME STORAGE FILE? (RA, DB)
-      # if not stored:
-      sum_data = Lapi::Summoner::FindByName.new(@summoner).call
-      @puuid = JSON.parse(sum_data)["puuid"] #or stored
 
-      matches_data = Lapi::Match::RecentIDs.new(@puuid, most_recent_match).call #TODO: Fix 
-      @ids = JSON.parse(matches_data)
-
-
-      match_client = Lapi::Match::ByID.new(@ids)
-      matches = []
-      match_client.batch_call do |body|
-        matches << user_match_data(body)
+      if @user = @storage_handler.get_user_by_summoner(@summoner)
+        @puuid = @storage_handler.get_puuid_by_user(@user)
+      else
+        sum_data = Lapi::Summoner::FindByName.new(@summoner).call
+        @puuid = JSON.parse(sum_data)["puuid"] 
+        @user = @storage_handler.add_user(@summoner, @puuid)
       end
 
-      print matches
+      store_matches(get_matches)
 
-
-
-      # File.open("match.json", 'w') {|file| file.write(match)}
-      # What are important values here? we need to check participantDTOs for user pos
-      # and find enemy laner in team pos, won or lost, game time?
-
-      # TODO: STORE PUUID, RETRIEVE MATCHES FROM PUUID
-      # TODO: STORE MATCHES
-      # TODO: CREATE COMMANDLINE UI
     end
 
     def most_recent_match
-      0
+      @storage_handler.get_timestamp
     end
 
     def user_match_data(string_match)
+
         match = JSON.parse(string_match)
         user_player ||= match['info']['participants'].find {|p| p['puuid'] == @puuid}
         user_player_lane = user_player['teamPosition']
@@ -63,6 +52,26 @@ module Leaguetrack
         user_data
     end
 
+    def get_matches
+      matches_data = Lapi::Match::RecentIDs.new(@puuid, most_recent_match).call
+      @ids = JSON.parse(matches_data)
+
+      match_client = Lapi::Match::ByID.new(@ids)
+      matches = []
+      match_client.batch_call do |body|
+        matches << user_match_data(body)
+      end
+
+      matches
+    end
+
+    def store_matches(matches) 
+      matches.each do |match|
+        @storage_handler.add_matchup(@user, match)
+      end
+
+      @storage_handler.write_data
+    end
 
   end
 end
---
+++ lib/leaguetrack/storage.rb
@@ -0,0 +1,55 @@
+module Leaguetrack
+  class Storage
+    def initialize(file_path = File.expand_path('../../data/leaguetrack_data.json', __dir__))
+      @file_path = file_path
+      @data = load
+    end
+
+    def load
+      if File.exist?(@file_path)
+        #TODO: add file template and load if leaguetrack_data is empty or doesnt exist
+        JSON.parse(File.read(@file_path), symbolize_names: true)
+      else
+        raise "File load error"
+      end
+    end
+
+    def update_timestamp(timestamp)
+      @data[:timestamp] = timestamp
+    end
+
+    def get_timestamp
+      @data.fetch(:timestamp, nil)
+    end
+
+    def get_user_by_puuid(puuid)
+      @data.fetch(:users, []).find { |user| user.fetch(:puuid, nil) == puuid } || false
+    end
+
+    def get_user_by_summoner(name)
+      @data.fetch(:users, []).find { |user| user.fetch(:summoner_name, nil) == name} || false
+    end
+
+    def get_puuid_by_user(user)
+      user.fetch(:puuid, nil)
+    end
+
+    def get_matchups(user)
+      user.fetch(:matchups, nil)
+    end
+
+    def add_matchup(user, matchup)
+      @data.fetch(:users, []).find {|u| u == user}[:matchups] << matchup
+    end
+
+    def add_user(summoner_name, puuid, matchups=[])
+      user = {:summoner_name => summoner_name, :puuid => puuid, :matchups => matchups}
+      @data[:users] << user
+      user
+    end
+
+    def write_data
+      File.write(@file_path, JSON.pretty_generate(@data))
+    end
+  end
+end
